861bbf74e29a9faf2ac699a918133acc6baa178e
David Griffiths <dogriffiths@gmail.com>
2013-03-29 12:16:02 +0000

    Now generates a random sequence

--- randpass.c
+++ randpass.c
@@ -2,10 +2,23 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/fcntl.h>
+#include <time.h>
 #include "randpass.h"
 
 int main()
 {
-  printf("Hello World!\n");
+  /*[ "$2" == "0" ] && CHAR="[:alnum:]" || CHAR="[:graph:]"
+    cat /dev/urandom | (LC_CTYPE=C tr -dc "$CHAR") | head -c ${1:-32}*/
+  int randomData = open("/dev/random", O_RDONLY);
+  int i;
+  for (i = 0; i < 32; i++) {
+    int myRandomInteger;
+    read(randomData, &myRandomInteger, sizeof myRandomInteger);
+    printf("%c", (abs(myRandomInteger) % 93) + 33);
+  }
+  printf("\n");
+  close(randomData);
   return 0;
 }
