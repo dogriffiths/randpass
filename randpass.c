a8514a09f578816620605443e735f988e79da852
David Griffiths <dogriffiths@gmail.com>
2013-03-29 11:59:41 +0000

    Simple hello world program

---
+++ randpass.c
@@ -0,0 +1,11 @@
+/* @(#)randpass.c
+ */
+
+#include <stdio.h>
+#include "randpass.h"
+
+int main()
+{
+  printf("Hello World!\n");
+  return 0;
+}
---
+++ randpass.h
@@ -0,0 +1,10 @@
+/* @(#)randpass.h
+ */
+
+#ifndef _RANDPASS_H
+#define _RANDPASS_H 1
+
+
+
+#endif /* _RANDPASS_H */
+
